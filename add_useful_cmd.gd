2e16ac2ebd07e47dd512494d213c6ffc087e980f
2439905184 <suchenglong018@126.com>
2021-07-20 13:18:20 +0800

    动态添加代码添加判断
    动态添加代码添加判断 防止输入空数据 导致错误bug

--- add_useful_cmd.gd
+++ add_useful_cmd.gd
@@ -4,4 +4,7 @@ func _on_btn_add_cmd_pressed():
 	var var_cmd=$vbox/var_cmd.text
 	var cmd=$vbox/cmd.text
 	var cmd_help=$vbox/cmd_help.text
-	emit_signal("add_cmd",var_cmd,cmd,cmd_help)
+	if var_cmd!="" and cmd!="" and cmd_help!="":
+		emit_signal("add_cmd",var_cmd,cmd,cmd_help)
+	else:
+		OS.alert("错误，你还有信息没有填写")
